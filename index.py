38ac6f62dfc9c04dbe9ccd35163bef56645a0751
David-9-age <davidumea20@gmail.com>
2025-12-07 19:17:34 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+#Taking inputs from users.
+print("Half Pattern Of Stars(*):  ")
+
+n = int(input("Enter the number of rows:  "))
+
+#Outer loop to handle number of rows.
+
+for i in range(n):
+    #iner loop
+    for j in range(i+1):
+        #display result
